5c9c32148b6216a4dd8dbf1a96a5dc37a15ae87b
Samyak <samyakpiya@gmail.com>
2024-12-07 23:20:22 -0500

    add shadcn/ui sonner component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,9 @@
+import NonDashboardNavbar from "@/components/NonDashboardNavbar";
+
 export default function Home() {
-  return <div className="">Hi</div>;
+  return (
+    <div className="nondashboard-layout">
+      <NonDashboardNavbar />
+    </div>
+  );
 }
---
+++ src/components/NonDashboardNavbar.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function NonDashboardNavbar() {
+  return <nav className="nondashboard-navbar">Hello</nav>;
+}
+
+export default NonDashboardNavbar;
