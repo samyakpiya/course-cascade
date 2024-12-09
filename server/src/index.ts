4403b3d6c91975b60ce85f03a04947b8c582cb53
Samyak <samyakpiya@gmail.com>
2024-12-09 08:35:53 -0500

    feat: setup course api with routes and controllers, reorganize seed files

--- server/src/index.ts
+++ server/src/index.ts
@@ -5,7 +5,9 @@ import cors from "cors";
 import helmet from "helmet";
 import morgan from "morgan";
 import * as dynamoose from "dynamoose";
+
 /* ROUTE IMPORTS */
+import courseRoutes from "./routes/courseRoutes";
 
 /* CONFIGURATIONS */
 dotenv.config();
@@ -29,6 +31,8 @@ app.use(cors());
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
+
+app.use("/courses", courseRoutes);
 
 /* SERVER */
 const port = process.env.PORT || 3000;
